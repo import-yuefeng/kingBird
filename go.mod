610d946655c3353119dc80175ba59a028d22ea58
import-yuefeng <zhuyuefeng0@gmail.com>
2019-11-25 08:44:44 +0800

    feat: 🎸 init project

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/import-yuefeng/kingBird
+
+go 1.12
+
+require (
+	github.com/gin-contrib/sse v0.0.0-20190905051334-43f0f29dbd2b
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v0.0.0-20191022195553-ed6926b37a63
+	github.com/json-iterator/go v0.0.0-20191112144728-44a7e7340d23
+	github.com/mattn/go-isatty v0.0.0-20191112051248-2a2f0ea997f9
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
+	github.com/modern-go/reflect2 v1.0.1
+	github.com/shawn1m/overture v0.0.0-20191110122731-d562aae15fde
+	github.com/sirupsen/logrus v1.4.1
+	github.com/ugorji/go v1.1.7
+	gopkg.in/go-playground/validator.v8 v8.18.2
+	gopkg.in/yaml.v2 v2.2.7
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+// The MIT License (MIT)
+// Copyright (c) 2019 import-yuefeng
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Package main is the entry point of whole program.
+
+package main
+
+import (
+	"flag"
+	"io"
+	"os"
+	"runtime"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	version = "0.0.1"
+
+	configPath = flag.String("c", "./config.json", "config file path")
+	logPath    = flag.String("l", "", "log file path")
+
+	isLogVerbose    = flag.Bool("v", false, "verbose mode")
+	processorNumber = flag.Int("p", runtime.NumCPU(), "number of processor to use")
+
+	isKinglet = flag.String("kinglet", "", "start kingBird which kinglet mode")
+	isKingctl = flag.String("kingctl", "", "start kingBird which kingctl mode")
+)
+
+func main() {
+	flag.Parse()
+	// parse command-line flag
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+
+	if *isLogVerbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		// except for program has Warn
+		log.SetLevel(log.FatalLevel)
+	}
+
+	if *logPath != "" {
+		lf, err := os.OpenFile(*logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0640)
+		if err != nil {
+			log.Errorf("Unable to open log file for writing: %s", err)
+		} else {
+			log.SetOutput(io.MultiWriter(lf, os.Stdout))
+		}
+	}
+
+	log.Infof("kingBird %s", version)
+	log.Info("If you need any help, please visit the project repository: https://github.com/import-yuefeng/kingBird")
+
+	runtime.GOMAXPROCS(*processorNumber)
+
+	if *isKingctl && *isKinglet == false {
+		kingctl.kingctl(*configPath)
+	} else if *isKinglet && *isKingctl == false {
+		kinglet.kinglet(*configPath)
+	} else {
+		log.Errorf("controlError")
+		return
+	}
+}
